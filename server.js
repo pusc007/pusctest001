bafa00588573fb09f631a8c9225f8e54305f5320
蔡信偉 <sinwei@n-way.net>
2021-05-24 21:23:44 +0800

    更新

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const path = require("path");
+//const path = require("path");
 
 const app = express();
-const __dirname = path.resolve();
-app.use(express.static("public"));
+//const __dirname = path.resolve();
+//app.use(express.static("public"));
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/public/aaa.html");
+  //res.sendFile(__dirname + "/public/aaa.html");
+  res.send("ssss");
 });
 app.get("/aaa", function (req, res) {
   res.send("ssss");

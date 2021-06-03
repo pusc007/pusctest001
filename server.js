e883ec3b031bf27a85718f3eb208df0570b260c1
蔡信偉 <sinwei@n-way.net>
2021-06-03 10:22:18 +0800

    更新

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const { ApolloServer, gql } = require("apollo-server-express");
+const depthLimit = require("graphql-depth-limit");
 const path = require("path");
 const app = express();
 //const __dirname = path.resolve();
@@ -11,6 +13,36 @@ app.get("/", function (req, res) {
 app.get("/aaa", function (req, res) {
   res.send("ssss");
 });
+const typeDefs = gql`
+  type Query {
+    test: String
+  }
+  type Mutation {
+    editTest(id: String): String
+  }
+`;
+const resolvers = {
+  Query: {
+    test: (root, {}, context) => {
+      return "aaa0";
+    },
+  },
+  Mutation: {
+    editTest: (root, { id }, context) => {
+      return "aaa1";
+    },
+  },
+};
+
+const noiseServer = new ApolloServer({
+  typeDefs: typeDefs,
+  resolvers: resolvers,
+  introspection: true,
+  playground: true,
+  context: ({ req }) => ({ token: req.headers["x-token"] }),
+  validationRules: [depthLimit(5)],
+});
+noiseServer.applyMiddleware({ app, path: "/api" });
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("啟動 http://localhost:5000/");
